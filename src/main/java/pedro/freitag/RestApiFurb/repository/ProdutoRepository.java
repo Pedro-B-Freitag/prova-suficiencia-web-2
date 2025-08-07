b4747b6fb9715898eb0ad122b3bd97f022977718
Pedro Bosini Freitag <127051349+Pedro-B-Freitag@users.noreply.github.com>
2025-08-06 23:42:28 -0300

    Revert "commit"
    This reverts commit f821330a2172a3857ceaed0f0116b266ada39d25.

--- src/main/java/pedro/freitag/RestApiFurb/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-
-package pedro.freitag.RestApiFurb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pedro.freitag.RestApiFurb.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {}
