9af8cecef125f9d2a57a805b79baf43b04cdbc69
Pedro Bosini Freitag <127051349+Pedro-B-Freitag@users.noreply.github.com>
2025-08-06 23:42:33 -0300

    Revert "Criação projeto"
    This reverts commit 50596f481d5f0eef44df78f2f8a968b8f6d02c22.

--- src/main/java/pedro/freitag/RestApiFurb/repository/ComandaRepository.java
+++
@@ -1,7 +0,0 @@
-
-package pedro.freitag.RestApiFurb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pedro.freitag.RestApiFurb.model.Comanda;
-
-public interface ComandaRepository extends JpaRepository<Comanda, Long> {}
