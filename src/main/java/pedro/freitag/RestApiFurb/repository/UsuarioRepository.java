0d6a31ae6d8248ee7b84f77bdaf42eccc92517a8
Pedro Bosini Freitag <127051349+Pedro-B-Freitag@users.noreply.github.com>
2025-08-06 22:48:07 -0300

    Revert "Criação projeto"
    This reverts commit 50596f481d5f0eef44df78f2f8a968b8f6d02c22.

--- src/main/java/pedro/freitag/RestApiFurb/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-
-package pedro.freitag.RestApiFurb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pedro.freitag.RestApiFurb.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
